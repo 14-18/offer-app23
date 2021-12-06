8e64bb4a75eeb4bf3c6455ff309b2f46d91564e7
jatin Bisht <jatinbisht1990@gmail.com>
2021-12-06 13:17:27 +0000

    Added readme file and updtae command

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
 
+// Prints "Hello World"
      System.out.println("Welcome to Git");
     }  
 }
---
+++ Readme.md
@@ -0,0 +1,8 @@
+This is a Offers application written in Java Language
+
+Developer Team: 
+Jatin
+Hitesh
+
+Application Description:
+This app will list the Offers of the products
