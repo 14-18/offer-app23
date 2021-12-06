b0a5b4a95e96a1b7e5acec70b771b31b6f273246
jatin Bisht <jatinbisht1990@gmail.com>
2021-12-06 13:08:58 +0000

    4th Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
+
+     System.out.println("Welcome to Git");
     }  
 }
 //This Java program will print my Message 
